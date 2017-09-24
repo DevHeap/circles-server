d3cfeeb3726f7b289206913ceb585008afe8db89
Mike Lubinets <lubinetsm@yandex.ru>
2017-09-24 14:18:07 +0300

    refactoring: Authenticator and UserDbUpdater

--- lib/circles-common/src/db/query.rs
+++ lib/circles-common/src/db/query.rs
@@ -20,3 +20,24 @@ impl Insert for PositionRecord {
         })
     }
 }
+
+impl Insert for User {
+    fn insert(self, pool: &AsyncPgPool) -> CpuFuture<usize, Error> {
+        use db::schema::users::dsl::*;
+        use futures::future::result;
+        use diesel::insert;
+        use diesel::prelude::*;
+        use diesel::pg::upsert::*;
+
+        // Insert an authentified user or, if user exists, just update 
+        pool.request(move |conn| {
+            result(
+                insert(
+                    &self.on_conflict(uid, do_update().set(&self.auth_data()))
+                ).into(users)
+                 .execute(&*conn)
+                 .map_err(Error::from)
+            )    
+        })
+    }
+}
